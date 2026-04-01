aef66734888180c0de644bcad99d6e4dc172e26b
daniyalkhan25 <80892999+daniyalkhan25@users.noreply.github.com>
2026-04-01 22:35:34 +0800

    Update restrictedspots.java

--- restrictedspots.java
+++ restrictedspots.java
@@ -1,3 +1,11 @@
 public class restrictedspots {
+    private int spotID;
+    private String spotName;
+    private double spotArea; // total area in m²
+    private double usableArea; // area available for occupancy
+    private int permittedAvgTime; // average wait/service time in minutes
+    private int maxCapacity;
+
+    public static final double Distancing_Area = 1.0; // m^2
 
 }
